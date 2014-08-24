a00719f71eca438d52deaf8e600549599775b607
Gris87 <Gris87@yandex.ru>
2014-08-24 12:13:53 +0400

    Button test

---
+++ Assets/Scripts/MainScene/ButtonScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour
+{
+	public void OnButtonClick()
+	{
+		rigidbody.velocity = new Vector3(0, 4, 0);
+	}
+}
