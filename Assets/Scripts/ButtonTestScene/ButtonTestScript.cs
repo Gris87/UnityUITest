ea0f351608c346150c832565728c0b852b311272
Gris87 <Gris87@yandex.ru>
2014-08-24 12:35:42 +0400

    ButtonTestScene

--- Assets/Scripts/ButtonTestScene/ButtonTestScript.cs
+++ Assets/Scripts/ButtonTestScene/ButtonTestScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class ButtonScript : MonoBehaviour
+public class ButtonTestScript : MonoBehaviour
 {
 	public void OnButtonClick()
 	{
---
+++ Assets/Scripts/Common/OpenSceneScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenSceneScript : MonoBehaviour
+{
+	public void openScene(string sceneName)
+	{
+		Application.LoadLevel(sceneName);
+	}
+}
