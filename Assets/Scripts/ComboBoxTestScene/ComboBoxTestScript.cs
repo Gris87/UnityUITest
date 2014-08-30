bfb63c8e69eca1019387aa51af851463ee543756
Gris87 <Gris87@yandex.ru>
2014-08-30 23:32:23 +0400

    ComboBox UI

---
+++ Assets/Scripts/ComboBoxTestScene/ComboBoxTestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class ComboBoxTestScript : MonoBehaviour, IPointerClickHandler
+{
+	GameObject scrollRect;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	public void OnPointerClick(PointerEventData data)
+	{
+	}
+}
