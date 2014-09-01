3ceb61fa6d8e3272e3f1f551609639683c2b7308
Gris87 <Gris87@yandex.ru>
2014-09-01 23:17:41 +0400

    TabWidgetTestScene

---
+++ Assets/Scripts/TabWidgetTestScene/TabClickScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class TabClickScript : MonoBehaviour
+{
+	public void OnTabClicked(bool isOn)
+	{
+		if (isOn)
+		{
+			Transform t = transform.parent;
+
+			for (int i=0; i<t.childCount; ++i)
+			{
+				if (t.GetChild(i) == transform)
+				{
+					TabWidgetTestScript script = FindInParents<TabWidgetTestScript>();
+					script.SetCurrentIndex(i);
+
+					break;
+				}
+			}
+		}
+	}
+
+	public T FindInParents<T>() where T : Component
+	{
+		var comp = gameObject.GetComponent<T>();
+		
+		if (comp != null)
+		{
+			return comp;
+		}
+		
+		Transform t = gameObject.transform.parent;
+		
+		while (t != null && comp == null)
+		{
+			comp = t.gameObject.GetComponent<T>();
+			t = t.parent;
+		}
+		
+		return comp;
+	}
+}
---
+++ Assets/Scripts/TabWidgetTestScene/TabWidgetTestScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TabWidgetTestScript : MonoBehaviour
+{
+	private Transform mTabs         = null;
+	private Transform mPages        = null;
+	private int       mCurrentIndex = -1;
+
+	// Use this for initialization
+	void Start()
+	{
+		mTabs = gameObject.transform.FindChild("Tabs");
+		
+		if (mTabs == null)
+		{
+			Debug.LogError("Tabs doesn't found in ComboBox");
+			return;
+		}
+
+		mTabs = mTabs.GetComponent<ScrollRect>().content.transform;
+
+		mPages = gameObject.transform.FindChild("Pages");
+		
+		if (mPages == null)
+		{
+			Debug.LogError("Pages doesn't found in ComboBox");
+			return;
+		}
+	}
+
+	public void UpdateTabs()
+	{
+	}
+
+	public void UpdateSelectedIndex()
+	{
+	}
+
+	public void SetCurrentIndex(int index)
+	{
+		mCurrentIndex = index;
+		UpdateSelectedIndex();
+	}
+	
+	public int GetCurrentIndex()
+	{
+		return mCurrentIndex;
+	}
+}
